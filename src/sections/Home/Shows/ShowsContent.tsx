c64e0cc66fcf3ec842eca429b524055239261d01
Mithunkumar <mithunkumari@karunya.edu.in>
2021-05-16 17:32:52 +0530

    feat: added carousel to shows section

--- src/sections/Home/Shows/ShowsContent.tsx
+++ src/sections/Home/Shows/ShowsContent.tsx
@@ -2,24 +2,26 @@ import React, { FC } from 'react';
 import { ShowsCard } from '../../../components/ShowsCard';
 import { ShowsProps } from './Shows.types';
 import { showsData } from './showsData';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 export const ShowsContent: FC<ShowsProps> = ({ className }) => {
     return (
         <div className={className}>
             <div className="shows-content-wrapper">
-                {showsData.map((data, idx) =>
-                    showsData.length - 1 === idx ? (
-                        <ShowsCard
-                            className="showscard-last"
-                            key={idx}
-                            name={data.name}
-                            tag={data.tag}
-                            image={data.image}
-                        />
-                    ) : (
-                        <ShowsCard key={idx} name={data.name} tag={data.tag} image={data.image} />
-                    )
-                )}
+                <Swiper
+                    spaceBetween={24}
+                    slidesPerView={'auto'}
+                    breakpoints={{
+                        600: {
+                            spaceBetween: 40,
+                        },
+                    }}>
+                    {showsData.map((data, idx) => (
+                        <SwiperSlide key={idx}>
+                            <ShowsCard key={idx} name={data.name} tag={data.tag} image={data.image} />
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
             </div>
         </div>
     );
