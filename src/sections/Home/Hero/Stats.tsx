5c2330c6ae0cb8400ae97b47e96ab6b4fe898087
Mithunkumar <mithunkumari@karunya.edu.in>
2021-05-17 09:48:49 +0530

    feat: added swipeable container to progressbar section

--- src/sections/Home/Hero/Stats.tsx
+++ src/sections/Home/Hero/Stats.tsx
@@ -2,21 +2,26 @@ import { FC } from 'react';
 import { ProgressBar } from '../../../components/ProgressBar';
 import { HeroProps } from './Hero.types';
 import { statsData } from './statsData';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 export const Stats: FC<HeroProps> = ({ className }) => {
     return (
         <div className={className}>
             <div className="stats-wrapper w-100">
                 <div className="stats-container w-100">
-                    {statsData.map((data, idx) => (
-                        <ProgressBar
-                            key={idx}
-                            icon={data.icon}
-                            label={data.label}
-                            percent={data.percent}
-                            value={data.value}
-                        />
-                    ))}
+                    <Swiper spaceBetween={20} slidesPerView={'auto'}>
+                        {statsData.map((data, idx) => (
+                            <SwiperSlide key={idx}>
+                                <ProgressBar
+                                    key={idx}
+                                    icon={data.icon}
+                                    label={data.label}
+                                    percent={data.percent}
+                                    value={data.value}
+                                />
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
                 </div>
             </div>
         </div>
